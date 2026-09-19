0d6688c60cc586369a78522af17ac67f60d71750
wsk4 <maa.gonzalezva@gmail.com>
2026-09-19 00:56:34 -0300

    feat(core): configure Axios HTTP client for microservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule,
+    HttpModule,
+  ],
 })
 export class AppModule {}
